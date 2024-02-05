97ed4b6f9b3a31a7367255222397c9384895a88c
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-05 20:37:31 +0100

    added documentation to the model classes.

--- lib/models/grocery_item.dart
+++ lib/models/grocery_item.dart
@@ -1,6 +1,10 @@
+// Import statement for the Category model.
 import 'package:shopping_list_app/models/category.dart';
 
+// Defines a GroceryItem object for use in the shopping list.
 class GroceryItem {
+  // Constructor for creating a GroceryItem with specified properties.
+  // Parameters are marked as required to ensure all necessary data is provided upon instantiation.
   const GroceryItem({
     required this.id,
     required this.name,
@@ -8,6 +12,7 @@ class GroceryItem {
     required this.category,
   });
 
+  // Properties of the GroceryItem class:
   final String id;
   final String name;
   final int quantity;
